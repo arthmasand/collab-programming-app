3ce72c10162be27902a9d53d2aebac10364a4799
arthmasand <arthhello@gmail.com>
2024-09-27 19:54:53 +0530

    entry visible on drizzle orm on room submission

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,21 +1,6 @@
-import NextAuth from "next-auth"
-import { db } from "@/db";
-import { DrizzleAdapter } from "@auth/drizzle-adapter";
-import GoogleProvider from "next-auth/providers/google";
-import type { Adapter } from "next-auth/adapters";
-import { config } from 'dotenv';
+import NextAuth from "next-auth";
+import { authConfig } from "@/lib/auth";
 
-// Load .env file
-config(); 
+const handler = NextAuth(authConfig);
 
-const handler = NextAuth({
-    adapter:DrizzleAdapter(db) as Adapter, 
-    providers: [
-        GoogleProvider({
-          clientId: process.env.GOOGLE_CLIENT_ID!,
-          clientSecret: process.env.GOOGLE_CLIENT_SECRET!,   
-        }),
-    ],    
-});
-
-export { handler as GET, handler as POST }  
+export { handler as GET, handler as POST };    
